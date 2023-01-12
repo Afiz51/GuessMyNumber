4a2e3499b8b7e5d8100f66df884c1d9b3ae1292e
Gboyega Lawal <afiz.lawal51@gmail.com>
2023-01-12 22:39:30 +0100

    added styling foe ios and andoid

--- guessMyNumber/screens/StartGameScreen.tsx
+++ guessMyNumber/screens/StartGameScreen.tsx
@@ -1,15 +1,40 @@
-import {TextInput, View} from "react-native"
-import PrimaryButton from "../components/PrimaryButton"
+import { TextInput, View, StyleSheet } from "react-native";
+import PrimaryButton from "../components/PrimaryButton";
 
 function StartGameScreen() {
-
-    return (
-        <View>
-            <TextInput />
-            <PrimaryButton>Reset</PrimaryButton>
-            <PrimaryButton>Confirm</PrimaryButton>
-        </View>
-    )
+  return (
+    <View style={styles.inputContainer}>
+      <TextInput style={styles.numberInput} maxLength={2} />
+      <PrimaryButton>Reset</PrimaryButton>
+      <PrimaryButton>Confirm</PrimaryButton>
+    </View>
+  );
 }
 
-export default StartGameScreen
+export default StartGameScreen;
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    marginTop: 100,
+    padding: 16,
+    backgroundColor: "#72063c",
+    marginHorizontal: 24,
+    borderRadius: 8,
+    elevation: 200,
+    shadowColor: "blue",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 6,
+    shadowRadius: 8,
+  },
+  numberInput: {
+    height: 50,
+    width: 50,
+    textAlign: "center",
+    fontSize: 32,
+    borderBottomColor: "#ddb52f",
+    borderBottomWidth: 2,
+    color: "#ddb52f",
+    marginVertical: 8,
+    fontWeight: "bold",
+  },
+});
